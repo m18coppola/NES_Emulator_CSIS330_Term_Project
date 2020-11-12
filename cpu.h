30b28ce4ad0c7db379c358b9a84a750aa8b7f9fa
ns30shel <ns30shel@siena.edu>
2020-11-12 18:24:09 -0500

    Added enum and cpu struct.

---
+++ cpu.h
@@ -0,0 +1,27 @@
+enum STATUS_FLAGS {
+	C = (1 << 0), /* Carry bit */
+	Z = (1 << 1), /* Zero flag */
+	I = (1 << 2), /* Disable interrupts */
+	D = (1 << 3), /* Decimal Mode */
+	B = (1 << 4), /* Break */
+	U = (1 << 5), /* Unused */
+	V = (1 << 6), /* Overflow */
+	N = (1 << 7), /* Negative */
+};
+
+typedef struct cpu CPU;
+typedef struct instruction INSTRUCTION;
+
+struct cpu {
+	/* Registers */
+	unsigned char a; 	/* Accumulator */
+	unsigned char x; 	/* X Register */
+	unsigned char y;	/* Y Register */
+	unsigned char stkp; 	/* Stack Pointer */
+	unsigned short pc;	/* Program Counter */
+	unsigned char status; 	/* Status Register */
+
+	/* Bus */
+	Bus* bus;
+}
+
