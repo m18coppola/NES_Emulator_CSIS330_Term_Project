b8ff136bba63fa728f7b6358692ad4fc9854f496
Michael Coppola <m18coppola@gmail.com>
2020-11-12 22:26:13 -0500

    added function prototypes to cpu.h for addressing modes and opcodes

--- cpu.h
+++ cpu.h
@@ -59,3 +59,34 @@ void cpu_nmi(CPU* cpu);     /* Non-maskable interrupt request. */
 
 /* Fetches memory according to opcode stored in the cpu. */
 void cpu_fetch(CPU* cpu);
+
+/* 6502 Addressing Modes */
+unsigned char IMP(CPU* cpu);  unsigned char IMM(CPU* cpu);	
+unsigned char ZP0(CPU* cpu);  unsigned char ZPX(CPU* cpu);	
+unsigned char ZPY(CPU* cpu);  unsigned char REL(CPU* cpu);
+unsigned char ABS(CPU* cpu);  unsigned char ABX(CPU* cpu);	
+unsigned char ABY(CPU* cpu);  unsigned char IND(CPU* cpu);	
+unsigned char IZX(CPU* cpu);  unsigned char IZY(CPU* cpu);
+
+/* 6506 Instruction Set */
+unsigned char ADC(CPU* cpu);	unsigned char AND(CPU* cpu);	unsigned char ASL(CPU* cpu);	unsigned char BCC(CPU* cpu);
+unsigned char BCS(CPU* cpu);	unsigned char BEQ(CPU* cpu);	unsigned char BIT(CPU* cpu);	unsigned char BMI(CPU* cpu);
+unsigned char BNE(CPU* cpu);	unsigned char BPL(CPU* cpu);	unsigned char BRK(CPU* cpu);	unsigned char BVC(CPU* cpu);
+unsigned char BVS(CPU* cpu);	unsigned char CLC(CPU* cpu);	unsigned char CLD(CPU* cpu);	unsigned char CLI(CPU* cpu);
+unsigned char CLV(CPU* cpu);	unsigned char CMP(CPU* cpu);	unsigned char CPX(CPU* cpu);	unsigned char CPY(CPU* cpu);
+unsigned char DEC(CPU* cpu);	unsigned char DEX(CPU* cpu);	unsigned char DEY(CPU* cpu);	unsigned char EOR(CPU* cpu);
+unsigned char INC(CPU* cpu);	unsigned char INX(CPU* cpu);	unsigned char INY(CPU* cpu);	unsigned char JMP(CPU* cpu);
+unsigned char JSR(CPU* cpu);	unsigned char LDA(CPU* cpu);	unsigned char LDX(CPU* cpu);	unsigned char LDY(CPU* cpu);
+unsigned char LSR(CPU* cpu);	unsigned char NOP(CPU* cpu);	unsigned char ORA(CPU* cpu);	unsigned char PHA(CPU* cpu);
+unsigned char PHP(CPU* cpu);	unsigned char PLA(CPU* cpu);	unsigned char PLP(CPU* cpu);	unsigned char ROL(CPU* cpu);
+unsigned char ROR(CPU* cpu);	unsigned char RTI(CPU* cpu);	unsigned char RTS(CPU* cpu);	unsigned char SBC(CPU* cpu);
+unsigned char SEC(CPU* cpu);	unsigned char SED(CPU* cpu);	unsigned char SEI(CPU* cpu);	unsigned char STA(CPU* cpu);
+unsigned char STX(CPU* cpu);	unsigned char STY(CPU* cpu);	unsigned char TAX(CPU* cpu);	unsigned char TAY(CPU* cpu);
+unsigned char TSX(CPU* cpu);	unsigned char TXA(CPU* cpu);	unsigned char TXS(CPU* cpu);	unsigned char TYA(CPU* cpu);
+
+
+/*
+ * Illegal operation capture (undefined opcodes)
+ * works as a NOP instruction
+ */
+unsigned char XXX(CPU* cpu);
